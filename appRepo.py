f73da11de13e3b57b1d4768ca43ac4ab7763a5c0
djjohnsongeek <danieleejohnson@gmail.com>
2023-11-29 19:15:56 -0500

    feat: working on report queries

--- appRepo.py
+++ appRepo.py
@@ -3,12 +3,19 @@ from datetime import datetime
 
 
 def log_view(user_uuid: str, source: str, page: str):
-    try:
-        PageView.insert(
-            timestamp = datetime.now(),
-            page = page,
-            user_uuid = user_uuid,
-            source = source
-        ).execute()
-    except:
-        pass
+    PageView.insert(
+        timestamp = datetime.now(),
+        page = page,
+        user_uuid = user_uuid,
+        source = source
+    ).execute()
+
+# select site views, determin how many are unique
+def site_views():
+    result = PageView.select(PageView, fn.Count(PageView.user_uuid).alias("count")).group_by(PageView.user_uuid).execute()
+
+# select all views, group by source, determine count for reach source
+
+# select index page views, determine how many are unique
+
+# select reports page views, determine how many are unique
